f06a57a68d181e3155d30ba706cf3bfa978d3bd1
Nate <natearmstrong2@gmail.com>
2013-04-10 23:27:52 -0600

    Undo last commit which added correct_domain function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :correct_domain!
-
-  def correct_domain!
-      unless request.host == 'www.rarecases.com'
-        redirect_to root_url, :status => 301  # or explicitly 'http://www.mysite.com/'
-      end
-    end
 end
